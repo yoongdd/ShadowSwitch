dcbdbf85ce3428b01b09f67d901a9b7de979ac7b
user <user@DESKTOP-QEO2AP2>
2023-03-08 14:40:14 +0900

    init commot

---
+++ ShadowSwitch/src/main/java/com/millionwere/ShadowSwitchApplication.java
@@ -0,0 +1,13 @@
+package com.millionwere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowSwitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowSwitchApplication.class, args);
+	}
+
+}
